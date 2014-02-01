784a297dea3c389d926f4c058b96e8c898a7c78f
Jesse Brown <jesse.brown@gmail.com>
2014-02-01 14:33:16 -0800

    Solution for step 4.

--- lib/sparsify.rb
+++ lib/sparsify.rb
@@ -2,14 +2,26 @@ module Sparsify
 
   DELIMETER = ENV['SPARSIFY_DELIMETER'] || '.'
 
-  def self.sparse(data, prefix=nil)
+  def self.sparse(data, options={})
+    @@delimeter = options.fetch(:separator, DELIMETER)
+    _sparse(data)
+  end
+
+  def self.unsparse(data, options={})
+    @@delimeter = options.fetch(:separator, DELIMETER)
+    _unsparse(data)
+  end
+
+  private
+
+  def self._sparse(data, prefix=nil)
 
     result = {}
 
     data.each do |key, value|
 
       if value.is_a?(Hash) && !value.empty?
-        result.merge! sparse(value, key_name(prefix, key))
+        result.merge! _sparse(value, key_name(prefix, key))
       else
         result.merge!({ key_name(prefix, key) => value })
       end
@@ -20,12 +32,12 @@ module Sparsify
 
   end
 
-  def self.unsparse(data)
+  def self._unsparse(data)
 
     result = {}
 
     data.each do |key, value|
-      names = key.split(DELIMETER)
+      names = key.split(@@delimeter)
       last_key = names.pop
 
       current = result
@@ -40,10 +52,8 @@ module Sparsify
 
   end
 
-  private
-
   def self.key_name(prefix, key)
-    [prefix, key].compact.join(DELIMETER)
+    [prefix, key].compact.join(@@delimeter)
   end
 
 end
