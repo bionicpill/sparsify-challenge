6f75bfb0008c5e44090fe0b91545f4010f9c61c7
Jesse Brown <jesse.brown@gmail.com>
2014-02-01 14:11:09 -0800

    Solution for step 1.

--- lib/sparsify.rb
+++ lib/sparsify.rb
@@ -1,5 +1,29 @@
-# encoding: utf-8
-
 module Sparsify
-  # Your implementation goes here
+
+  DELIMETER = ENV['SPARSIFY_DELIMETER'] || '.'
+
+  def self.sparse(data, prefix=nil)
+
+    result = {}
+
+    data.each do |key, value|
+
+      if value.is_a?(Hash)
+        result.merge! sparse(value, key_name(prefix, key))
+      else
+        result.merge!({ key_name(prefix, key) => value })
+      end
+
+    end
+
+    result
+
+  end
+
+  private
+
+  def self.key_name(prefix, key)
+    [prefix, key].compact.join(DELIMETER)
+  end
+
 end
